3165f3d4782cda52fe8adee5b54563abf22af36f
natolotrakeziatsarazaza-cell <natolotrakeziatsarazaza@gmail.com>
2025-08-13 14:07:15 +0200

    bonjour

---
+++ acceuil.php
@@ -0,0 +1,59 @@
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Acceuil\suivi-evaluation</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css\information.css">
+</head>
+<body>
+
+  <header>
+    <h1>SUIVI-EVALUATION</h1>
+  </header>
+
+  <div class="barre">
+    <a href="liste.php">Activite</a>
+    <a href="ajout_enseignant.php">Objectif</a>
+    <a href="index.php">Déconnexion</a>
+  </div>
+
+  <div class="content">
+    <h2>Bienvenue</h2>
+
+    <div class="grid">
+      <div class="card">
+        <h3>MISSION</h3>
+        
+      </div>
+      <div class="card">
+        <h3>ACTIVITES</h3>
+        
+      </div>
+      <div class="card">
+        <h3>EVALUTION ANNUELLE</h3>
+        
+      </div>
+    </div>
+  </div>
+  <div class="tableau-bord">
+      <h1>TABLEAU DE BORD</h1>
+    <div class="info">
+      <div class="card">
+        <h3>Taux de Réalisation</h3>
+        <span class="status success">Bon</span>
+      </div>
+    <div class="card">
+      <h3>Activités Réalisées</h3>
+      <span class="status warning">En cours</span>
+    </div>
+    <div class="card">
+        <h3>Retards</h3>
+          <span class="status danger">Critique</span>
+    </div>
+  </div>
+  <div class="statistique">
+      <h1>STATISTIQUE</h1>
+  </div> 
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login/suivi-Evaluation</title>
+    <link rel="stylesheet" href="css\login.css">
+</head>
+<body>
+    <form action="index.php">
+        <p>Bienvenue dans la plateforme de suivi stratégique!!!</p>
+        <div class="login">
+        <h1>LOGIN</h1><br>
+        <input type="text" placeholder="Votre Nom"><br><br>    
+        <input type="text" placeholder="Votre e-mail"><br><br>
+        
+        <input type="password" placeholder="Mots de passe"><br><br>
+        <section class="checkbox"><input type="checkbox">Se souvenir de moi</section>
+        <br><button>SE CONNECTER</button><br><br>
+        <a href="registre.php">Creer un compte</a>
+    </div>
+
+    </form>
+    
+</body>
+</html>
